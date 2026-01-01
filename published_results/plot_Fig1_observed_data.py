a4e18dac6bad63bbf17dd7d0d21425cbeae6fd85
Samson Leong (wkstn) <55839002+SSL32081@users.noreply.github.com>
2026-01-01 18:46:43 +0800

    Recreate GRB plot

--- published_results/plot_Fig1_observed_data.py
+++ published_results/plot_Fig1_observed_data.py
@@ -2,9 +2,98 @@
 from pathlib import Path
 import numpy as np
 import pandas as pd
-
+from astropy.coordinates import SkyCoord
+import astropy.units as u
+import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm
+plt.style.use('../matplotlibrc')
+FIG_DIR = Path("../figures/")
 DATA_DIR = Path("../data/")
 
 
-# Observed GW skyloc maps
-observed_map = np.load(DATA_DIR / 'GWTC-4_mixed_combined_skymap.npy')
+## GRB 
+def read_grb_data(file_path):
+    # Load the data
+    keys = ('ra', 'dec', 'pos_err', 'duration', 'redshift')
+    dtypes = [(key, 'f8') for key in keys]
+    arr = np.loadtxt(file_path, dtype=dtypes, usecols=(3,4,5,6,11))
+
+    # Pre-process data
+    mask = np.ones_like(arr, dtype=bool)
+    for key in keys:
+        mask &= np.isfinite(arr[key]) & (arr[key] != -999)
+    arr = arr[mask]
+
+    # Convert RA, Dec to radians for Mollweide projection
+    # SL: How is this different from just np.radians?
+    coords = SkyCoord(ra=arr['ra'] * u.deg, dec=arr['dec'] * u.deg, frame="icrs")
+    arr['ra'] = (360.0 * u.deg - coords.ra).wrap_at(180 * u.deg).radian
+    arr['dec'] = coords.dec.radian
+    return arr
+
+
+def read_grb_simulated_data(file_path):
+    with open(file_path) as f:
+        header = f.readlines()[0]
+        # Strip away the leading comment and closing newline character
+        keys = header[2:-2].split(' ')
+        dtypes = [(key, 'f8') for key in keys]
+    simulated_grbs = np.loadtxt(file_path, dtype=dtypes)
+    return simulated_grbs
+
+
+def plot_grb_skymap(grb_data, ax=plt.gca(), fig=plt.gcf()):
+    # Separate long and short GRBs
+    short_mask = grb_data['duration'] >= 2.0
+    long_mask = grb_data['duration'] >= 2.0
+    short_grbs = grb_data[short_mask]
+    long_grbs = grb_data[long_mask]
+    
+    log_norm = LogNorm(vmin=2, vmax=350)
+    sc = ax.scatter(long_grbs['ra'], long_grbs['dec'], 
+                      c=long_grbs['duration'], s=10, marker="o",
+                      cmap="winter", norm=log_norm, alpha=0.9,
+                      edgecolors="none", label="Long GRBs", 
+                      rasterized=True)
+    ax.scatter(short_grbs['ra'], short_grbs['dec'], 
+                 s=18, c='red', marker="+",
+                 linewidths=1.1, alpha=0.9,
+                 label="Short GRBs", rasterized=True)
+
+    fig.colorbar(sc, ax=ax, pad=0.05, shrink=0.7, 
+                 orientation="vertical", 
+                 label="Long GRB Burst Duration (s)")
+
+    ax.set_xticklabels([])
+    ax.set_yticklabels([])
+    ax.grid(True, linestyle="-", linewidth=1, alpha=0.5)
+    ax.set_xticks(np.radians(np.linspace(-180, 180, 13)))  # Fewer longitude lines
+    ax.set_yticks(np.radians(np.linspace(-90, 90, 7)))    # Fewer latitude lines
+    ax.axhline(y=0, linewidth=0.8, linestyle="-", alpha=0.5)  # Equator
+    ax.axvline(x=0, linewidth=0.8, linestyle="-", alpha=0.5)  # Prime meridian
+
+    ax.set_title("GRB Skymap", pad=16, fontsize=14)
+    ax.legend(loc='lower left', bbox_to_anchor=(0.68, -0.07),
+              ncol=2, fancybox=False)
+    return fig, ax
+
+
+
+def main():
+
+    # Observed GW skyloc maps
+    observed_map = np.load(DATA_DIR / 'GWTC-4_mixed_combined_skymap.npy')
+    # GRB locations
+    grb_data = read_grb_data(DATA_DIR / "GRB_Summary_table.txt")
+    sim_grb_data = read_grb_simulated_data(DATA_DIR / "simulated_grbs.txt")
+
+    fig, axes = plt.subplots(3, 1, figsize=(4, 8), 
+                             constrained_layout=True, subplot_kw={'projection': 'mollweide'})
+
+    plot_grb_skymap(grb_data, ax=axes[1], fig=fig)
+
+    fig.savefig(FIG_DIR / 'Fig1_observed_data.png', dpi=300)
+
+
+if __name__ == "__main__":
+    main()
