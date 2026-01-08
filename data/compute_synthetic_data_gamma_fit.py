f18185d5e016ac6bc18b0687486658acedc1965f
Samson Leong (wkstn) <55839002+SSL32081@users.noreply.github.com>
2026-01-09 04:13:15 +0800

    Add options to gamma fit and Fig6 plot

--- data/compute_synthetic_data_gamma_fit.py
+++ data/compute_synthetic_data_gamma_fit.py
@@ -2,15 +2,25 @@
 import numpy as np
 from scipy.stats import gamma 
 from multiprocessing import Pool
+import argparse
 import sys
 sys.path.append('../published_results/')
-from utils import DATA_DIR, compute_correlation_function
+from utils import DATA_DIR, CF_LMAX, compute_correlation_function
+
+parser = argparse.ArgumentParser(description='Options for computing the gamma fit of synthetic data.')
+parser.add_argument('--ntheta', type=int, default=180,
+                    help='Number of theta bins for correlation function computation.')
+parser.add_argument('--lmax', type=int, default=CF_LMAX, 
+                    help='Maximum multipole moment lmax for correlation function computation.')
+parser.add_argument('--windowed', action='store_true',
+                    help='Whether to apply a resolution-limited window function in the correlation function computation.')
 
 
 def _gamma_fit(synth_data_row):
-    scale = 1e12  # Regularising values for better fitting.
+    scale = 1e13  # Regularising values for easier fitting.
     fitted_params = gamma.fit(synth_data_row * scale, method='MLE')
-    return gamma.mean(*fitted_params) / scale, gamma.std(*fitted_params) / scale, np.mean(synth_data_row), np.std(synth_data_row)
+    return gamma.mean(*fitted_params) / scale, gamma.std(*fitted_params) / scale, \
+        np.mean(synth_data_row), np.std(synth_data_row)
 
 
 def mp_gamma_fit(synth_data):
@@ -19,33 +29,42 @@ def mp_gamma_fit(synth_data):
     return np.array(results, dtype=[('gamma_mean', 'f8'), ('gamma_std', 'f8'), ('mean', 'f8'), ('std', 'f8')])
 
 
-def recompute_correlation_function(cl_data, cf_data, nthetas=500):
+def recompute_correlation_function(cl_data, cf_data, lmax, windowed=False, nthetas=500):
     if cf_data.shape[1] == nthetas:
         return cf_data.T
     print('Resizing correlation function data...')
     thetas = np.linspace(0, np.pi, nthetas)
     output_cf = []
     for spectrum in cl_data:
-        cf = compute_correlation_function(spectrum, thetas, lmax=128)
+        cf = compute_correlation_function(
+            spectrum, thetas, lmax=lmax, windowed=windowed)
         output_cf.append(cf)
     return np.array(output_cf).T
 
 
 def main():
-    NTHETAS = 180
+    args = parser.parse_args()
+    NTHETAS = args.ntheta
+    LMAX = args.lmax
+    WINDOWED = args.windowed
+
+    suffix = f"n{NTHETAS:d}_lmax{LMAX:d}"
+    if WINDOWED:
+        suffix += "_windowed"
+
     # Read synthetic data
-    gw_synth_data = np.load(DATA_DIR / 'congregated_synthetic_gw_correlation_stats_1000_85_ellmax_128_n1000.npy')
+    gw_synth_data = np.load(DATA_DIR / 'congregated_synthetic_gw_correlation_stats_1000_85_lmax128_n1000.npy')
     print('(Multi-)Processing gamma fit for synthetic GW correlations...')
     gw_fit_results = mp_gamma_fit(
         recompute_correlation_function(
             gw_synth_data['multipole_spectrum'],
             gw_synth_data['angular_spectrum'],
-            nthetas=NTHETAS
+            lmax=LMAX, windowed=WINDOWED, nthetas=NTHETAS 
         )
     )
-    np.save(DATA_DIR / 'synthetic_gw_correlation_CF_gamma_fit.npy', gw_fit_results)
+    np.save(DATA_DIR / f'synthetic_gw_correlation_CF_gamma_fit_{suffix}.npy', gw_fit_results)
 
-    grb_synth_data = np.load(DATA_DIR / 'congregated_synthetic_grb_correlation_stats_100_lmax128_n1000.npy')
+    grb_synth_data = np.load(DATA_DIR / 'congregated_synthetic_grb_correlation_stats_1000_lmax128_n1000.npy')
     print('(Multi-)Processing gamma fit for synthetic GRB correlations...')
     grb_gamma_fits = {}
     for grb_type in ('full', 'short', 'long'):
@@ -54,11 +73,11 @@ def main():
             recompute_correlation_function(
                 grb_synth_data[f'{grb_type}_multipole_spectrum'],
                 grb_synth_data[f'{grb_type}_angular_spectrum'],
-                nthetas=NTHETAS
+                lmax=LMAX, windowed=WINDOWED, nthetas=NTHETAS 
             )
         )
 
-    np.savez(DATA_DIR / 'synthetic_grb_correlation_CF_gamma_fit.npz', **grb_gamma_fits)
+    np.savez(DATA_DIR / f'synthetic_grb_correlation_CF_gamma_fit_{suffix}.npz', **grb_gamma_fits)
 
 
 if __name__ == "__main__":
--- published_results/plot_Fig6_autocorrelation.py
+++ published_results/plot_Fig6_autocorrelation.py
@@ -1,57 +1,194 @@
 #!/usr/bin/env python3
-import os
-from pathlib import Path
 import numpy as np
-from scipy.special import lpmv  # Associated Legendre polynomials
+import argparse
 import matplotlib.pyplot as plt
-from utils import DATA_DIR, FIG_DIR, SINGLE, DPI, \
-    read_grb_data, compute_correlation_function
+import matplotlib.lines as mlines
+from utils import DATA_DIR, FIG_DIR, SINGLE, DPI, NSIDE, CF_LMAX, \
+    read_grb_data, compute_correlation_function, compute_skymap_from_points
 import healpy as hp
-from matplotlib.colors import Normalize
 
-LMAX = 26
-NSIDE = 256
+
+parser = argparse.ArgumentParser(description='Options for computing the gamma fit of synthetic data.')
+parser.add_argument('--ntheta', type=int, default=180,
+                    help='Number of theta bins for correlation function computation.')
+parser.add_argument('--lmax', type=int, default=CF_LMAX, 
+                    help='Maximum multipole moment lmax for correlation function computation.')
+parser.add_argument('--windowed', action='store_true',
+                    help='Whether to apply a resolution-limited window function in the correlation function computation.')
+parser.add_argument('--gammafit', action='store_true',
+                    help='Whether to use gamma fit results for plotting.')
+
+
+DEG2RAD = np.pi / 180.0
 NPIX = hp.nside2npix(NSIDE)
 
-def plot_glade_correlation(glade_data, fig, ax):
-    print("Computing angular power spectrum...")
-    cl_obs = hp.anafast(glade_data, lmax=LMAX)
+args = parser.parse_args()
+LMAX = args.lmax
+NTHETAS = args.ntheta
+WINDOWED = args.windowed
+USE_GAMMA_FIT = args.gammafit
+mean_key = 'mean'
+std_key = 'std'
+if USE_GAMMA_FIT:
+    mean_key = 'gamma_mean'
+    std_key = 'gamma_std'
 
-    # Define theta range for correlation function 
-    theta_degrees = np.linspace(0.0, 180.0, 10000)
-    theta_degrees = np.sort(theta_degrees)
+
+def shift_exponential_text(ax):
+    exp = ax.yaxis.get_offset_text()
+    exp.set_x(-0.05)
+    return exp
+
+
+def plot_glade_correlation(glade_data, thetas, ax):
+    print("Compute map for GLADE+ data...")
+    galaxy_map = compute_skymap_from_points(
+        glade_data['l_gal'], glade_data['b_gal'], NSIDE
+    )
+
+    print("Computing angular power spectrum...")
+    cl_obs = hp.anafast(galaxy_map, lmax=CF_LMAX)
 
     # Compute correlation function
     print("Computing angular correlation function...")
-    C_theta_obs = compute_correlation_function(cl_obs, theta_degrees, LMAX)
+    C_theta_obs = compute_correlation_function(
+        cl_obs, thetas * DEG2RAD, LMAX, windowed=WINDOWED)
 
-    ax.plot(theta_degrees, C_theta_obs, 'C3', label='GLADE+ Galaxies')
+    ax.plot(thetas, C_theta_obs, 'C3', label='GLADE+ Galaxies')
 
     ax.set_xlabel(r'Angular separation $\theta$ [degrees]')
-    ax.set_ylabel(r'Correlation function C($\theta$)')
+    ax.set_ylabel(r'Autocorrelation function $C(\theta)$')
     # ax.set_ylim(1e-14,1e-10) #NOTE: this may affect visual interpretation though
-    ax.set_title('Angular Correlation Function: GLADE+ Galaxy Distribution')
-    ax.set_yscale('log')
+    ax.set_title('GLADE+ Galaxy Distribution')
+    # ax.set_yscale('log')
     ax.grid(True, which='both', ls='--', lw=0.5)
-    ax.legend()
+    # ax.legend()
+    shift_exponential_text(ax)
+
+
+def plot_gw_correlation(gw_skymap, gw_synth_stat, thetas, ax):
+    # Observed data
+    print("Computing angular power spectrum...")
+    # gw_map_ring = hp.reorder(gw_data, n2r=True)
+    cl_obs = hp.anafast(gw_skymap, lmax=CF_LMAX)
+    C_theta_obs = compute_correlation_function(
+        cl_obs, thetas * DEG2RAD, LMAX, windowed=WINDOWED)
+    ax.plot(thetas, C_theta_obs, 'C3', zorder=10, label='Observed GW Events')
+    
+    colour = 'k'
+    alpha_dict = { 1:  0.5, 2:  0.35, 3:  0.15 }
+
+    thetas = np.linspace(0.0, 180.0, gw_synth_stat[mean_key].size)
+    for n_sigma in reversed(range(1, 4)):
+        ax.fill_between(
+            thetas, 
+            gw_synth_stat[mean_key] - n_sigma * gw_synth_stat[std_key],
+            gw_synth_stat[mean_key] + n_sigma * gw_synth_stat[std_key], 
+            alpha=alpha_dict[n_sigma],
+            color=colour, linewidth=0)
+
+    # Plot mean correlation function
+    ax.plot(thetas, gw_synth_stat[mean_key], 'C0', label=f'Synthetic GW')
+
+    # ax.set_xlabel(r'Angular separation $\theta$ [degrees]')
+    ax.set_ylabel(r'Autocorrelation function $C(\theta)$')
+    ax.set_title('Synthetic vs Observed GW Skymaps')
+    ax.grid(True, which='both', ls='--', lw=0.5)
+    ax.legend(framealpha=0.5)
+    shift_exponential_text(ax)
+
+
+def plot_grb_correlation(grb_data, grb_synth_stats, thetas, ax):
+    obs_grb_dict = {
+        'full': grb_data,
+        'short': grb_data[grb_data['duration'] < 2.0],
+        'long': grb_data[grb_data['duration'] >= 2.0]
+    }
+
+    colour_dict = { 'full': 'grey', 'short': 'C1', 'long': 'C2' }
+    line_colour_dict = { 'full': 'k', 'short': 'darkorange', 'long': 'darkgreen' }
+    alpha_dict = { 1:  0.5, 2:  0.35, 3:  0.15 }
+
+    for grb_type in ('full', 'short', 'long'):
+        # Convert GRB data to HEALPix map
+        print("Compute map for GRB data...")
+        grb_dataset = obs_grb_dict[grb_type]
+        grb_map = compute_skymap_from_points(grb_dataset['l_gal'], grb_dataset['b_gal'], NSIDE)
+        print("Computing angular power spectrum...")
+        cl_obs = hp.anafast(grb_map, lmax=CF_LMAX)
+        print("Computing angular correlation function...")
+        C_theta_obs = compute_correlation_function(
+            cl_obs, thetas * DEG2RAD, LMAX, windowed=WINDOWED)
+        # Plot observed data
+        thetas = np.linspace(0.0, 180.0, C_theta_obs.size)
+        ax.plot(thetas, C_theta_obs, line_colour_dict[grb_type], zorder=10)
+
+        # Synthetic data
+        mean, std = grb_synth_stats[f'{grb_type}_grb_gamma_fit'][mean_key], \
+            grb_synth_stats[f'{grb_type}_grb_gamma_fit'][std_key]
+        
+        thetas = np.linspace(0.0, 180.0, mean.size)
+        for n_sigma in reversed(range(1, 4)):
+            ax.fill_between(
+                thetas, mean - n_sigma * std, mean + n_sigma * std, 
+                alpha=alpha_dict[n_sigma], color=colour_dict[grb_type], 
+                linewidth=0)
+
+        # Plot mean correlation function
+        ax.plot(thetas, mean, line_colour_dict[grb_type], linestyle='--')
+
+    # ax.set_xlabel(r'Angular separation $\theta$ [degrees]')
+    ax.set_ylabel(r'Autocorrelation function $C(\theta)$')
+    ax.set_title('Synthetic vs Observed GRB Skymaps')
+    ax.grid(True, which='both', ls='--', lw=0.5)
+    shift_exponential_text(ax)
+
+    h1 = [
+        mlines.Line2D([], [], color='k', linestyle='-', label='Observed GRBs'), 
+        mlines.Line2D([], [], color='grey', linestyle='--', label='Synthetic GRBs'), 
+    ]
+    h2 = [ 
+        mlines.Line2D([], [], color='k', linestyle='-', label='All GRBs'), 
+        mlines.Line2D([], [], color='darkorange', linestyle='-', label='Short GRBs'), 
+        mlines.Line2D([], [], color='darkgreen', linestyle='-', label='Long GRBs'), 
+    ]
+    leg1 = ax.legend(handles=h1, loc='upper right', framealpha=0.5)
+    ax.add_artist(leg1)
+    ax.legend(handles=h2, loc='right', framealpha=0.5)
+
+    return ax
 
 
 def main():
+    suffix = f"n{NTHETAS:d}_lmax{LMAX:d}"
+    if WINDOWED:
+        suffix += "_windowed"
+
     # Read GW correlations
-    gw_corr = np.load(DATA_DIR / 'congregated_synthetic_GW_correlation_stats.npz')
+    gw_skymap = np.load(DATA_DIR / 'GWTC4p0_combined_galactic_skymap.npy')
+    gw_synth_fit = np.load(DATA_DIR / f'synthetic_gw_correlation_CF_gamma_fit_{suffix}.npy')
     # Read GRB correlations
-    grb_corr = np.load(DATA_DIR / 'congregated_synthetic_grb_correlation_stats.npz')
+    grb_data = read_grb_data(DATA_DIR / 'GRB_Summary_table.txt')
+    grb_synth_fit = np.load(DATA_DIR / f'synthetic_grb_correlation_CF_gamma_fit_{suffix}.npz')
     # Read GLADE+ data
     glade_data = np.load(DATA_DIR / 'GLADE_galactic_coords.npy')
 
-    fig, axes = plt.subplots(3, 1, figsize=(SINGLE, 15), constrained_layout=True)
+    # Use the same theta values for all plots
+    theta_degs = np.linspace(0.0, 180.0, int(1e4))
 
-    ax = axes[0]
-    
+    fig, axes = plt.subplots(3, 1, figsize=(SINGLE, 7), 
+                             sharex=True, height_ratios=[1, 1.5, 1])
 
-    fig.savefig(FIG_DIR / 'Fig6_autocorrelations.pdf', dpi=DPI)
-    return fig
+    plot_gw_correlation(gw_skymap, gw_synth_fit, theta_degs, axes[0])
+    plot_grb_correlation(grb_data, grb_synth_fit, theta_degs, axes[1])
+    plot_glade_correlation(glade_data, theta_degs, axes[2])
+    axes[2].set_xlim(0, 180)
+
+    if USE_GAMMA_FIT:
+        suffix += '_gammafit'
+    fig.savefig(FIG_DIR / f"Fig6_autocorrelations_{suffix}.pdf", dpi=DPI)
+    return fig, axes
 
 
 if __name__ == '__main__':
-    main()
+    fig, axes = main()
