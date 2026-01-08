eedcd3675f77cced3526bd75d6bbf64d56dec6ca
Samson Leong (wkstn) <55839002+SSL32081@users.noreply.github.com>
2026-01-09 04:15:55 +0800

    add tests for different config of correlation functions

---
+++ tests/test_configs_for_Fig6.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env zsh
+
+source /cvmfs/software.igwn.org/conda/etc/profile.d/conda.sh
+conda activate gw-school-2025
+
+DATA_DIR='../data'
+PLOT_DIR='../published_results'
+
+for nths in 180 500; do
+    for lmax in 26 128; do
+        echo "Onto nthetas = ${nths} and lmax = ${lmax}"
+        cd ${DATA_DIR}
+        python compute_synthetic_data_gamma_fit.py --ntheta ${nths} --lmax ${lmax} 
+        python compute_synthetic_data_gamma_fit.py --ntheta ${nths} --lmax ${lmax} --windowed
+        cd ${PLOT_DIR}
+        python plot_Fig6_autocorrelation.py --ntheta ${nths} --lmax ${lmax} & 
+        python plot_Fig6_autocorrelation.py --ntheta ${nths} --lmax ${lmax} --windowed & 
+        python plot_Fig6_autocorrelation.py --ntheta ${nths} --lmax ${lmax} --gammafit &
+        python plot_Fig6_autocorrelation.py --ntheta ${nths} --lmax ${lmax} --windowed --gammafit &
+    done
+done
+
+echo "Waiting all jobs to complete"
+wait
+
+echo "All done, exiting..."
