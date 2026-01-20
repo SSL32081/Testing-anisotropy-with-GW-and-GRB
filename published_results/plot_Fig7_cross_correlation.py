34fc7bbc42b17d1ebe23b6514a836b9e59334da3
Samson Leong (wkstn) <55839002+SSL32081@users.noreply.github.com>
2026-01-21 00:09:20 +0800

    Add Fig 7

--- published_results/plot_Fig7_cross_correlation.py
+++ published_results/plot_Fig7_cross_correlation.py
@@ -1,59 +1,66 @@
 #!/usr/bin/env python3
-import os
-from pathlib import Path
 import numpy as np
-from scipy.special import lpmv  # Associated Legendre polynomials
 import matplotlib.pyplot as plt
-from utils import DATA_DIR, FIG_DIR, SINGLE, DPI, \
-    read_grb_data, compute_correlation_function
 import healpy as hp
-from matplotlib.colors import Normalize
 
-
-def corr_func(theta, Cl):
-    c = 0
-    x = np.cos(theta)
-    for ell in range(len(Cl)):
-        c += (1 + 2 * ell) * Cl[ell] * legendre(ell)(x)
-    return c / (4 * np.pi)
+from utils import DATA_DIR, FIG_DIR, DPI, SINGLE, CF_LMAX, NSIDE, \
+    read_grb_data, compute_skymap_from_points, compute_correlation_function
 
 
 def main():
     # Read GW correlations
     gw_skymap = np.load(DATA_DIR / 'GWTC4p0_combined_galactic_skymap.npy')
     # Read GRB correlations
-    grb_corr = np.load(DATA_DIR / 'congregated_synthetic_grb_correlation_stats.npz')
+    grb_data = read_grb_data(DATA_DIR / "GRB_Summary_table.txt")
 
-    fig, ax = plt.subplots(1, 1, figsize=(SINGLE, 2.1), constrained_layout=True)
+    obs_grb_dict = {
+        'all': (grb_data, 'k', '--'),
+        'short': (grb_data[grb_data['duration'] < 2.0], 'C1', '-'),
+        'long': (grb_data[grb_data['duration'] >= 2.0], 'C2', '-')
+    }
 
-    if list_depth(Cls) >= 1:
-        if Cl_labels is None:
-            Cl_labels = [None] * len(Cls)
-        if colors is None:
-            colors = [None] * len(Cls)
-        if linestyles is None:
-            linestyles = [None] * len(Cls)
-        for Cl, label, color, ls in zip(Cls, Cl_labels, colors, linestyles):
-            ax.plot(theta_vals, corr_func(np.radians(theta_vals), Cl),
-                    label=label, color=color, linestyle=ls)
-    else:
-        ax.plot(theta_vals, corr_func(np.radians(theta_vals), Cls),
-                label=Cl_labels, color=colors, linestyle=linestyles)
+    theta_degs = np.linspace(0.0, 180.0, int(1e4))
 
-    if xlog:
-        ax.set_xscale('log')
-    if ylog:
-        ax.set_yscale('log')
+    fig, ax = plt.subplots(1, 1, figsize=(SINGLE, 3.1))
+    ax2 = ax.inset_axes([15, 0.18e-12, 165, 1.3e-12], transform=ax.transData)
 
-    ax.set_xlabel(r'$\theta\,/\,\textrm{deg}$')
-    ax.set_ylabel(ylabel if ylabel is not None else r'Correlation, $C(\theta)$')
-    ax.set_title(title if title is not None else r'Angular Correlation Function $C(\theta)$')
-    ax.legend()
-    ax.grid(True)
+    for grb_type in ('all', 'short', 'long'):
+        grb_dataset, colour, linestyle = obs_grb_dict[grb_type]
+        grb_map = compute_skymap_from_points(-grb_dataset['l_gal'], grb_dataset['b_gal'], NSIDE)
+        grb_map /= grb_map.sum()
+        print("Computing angular power spectrum...")
+        cross_cl = hp.anafast(gw_skymap, map2=grb_map, lmax=CF_LMAX)
+        # Tested, this version does not make a difference:
+        # cross_cl = hp.anafast(hp.alm2map(hp.map2alm(gw_skymap / gw_skymap.sum(), lmax=CF_LMAX), nside=NSIDE),
+        #                       map2=hp.alm2map(hp.map2alm(grb_map, lmax=CF_LMAX), nside=NSIDE), lmax=CF_LMAX)
+        cross_cf = compute_correlation_function(cross_cl, theta_degs * np.pi / 180, CF_LMAX, windowed=True)
 
-    if save:
-        filename = f'{title}{save_ext}'.replace(" ", "")
-        fig.savefig(filename, dpi=600)
+        label = grb_type.capitalize() + ' GRB x GW'
+        ax.plot(theta_degs, cross_cf, c=colour, linestyle=linestyle)
+        ax2.plot(theta_degs, cross_cf, c=colour, linestyle=linestyle, label=label)
+
+    ax.set_ylim(0, 1.69e-12)
+    ax2.set_xlim(-5, 185)
+
+    indicator = ax.indicate_inset_zoom(ax2, edgecolor="grey")
+    # Change the connecting lines
+    for vis, path in zip((False, True, True, False), indicator[1]):
+        path.set_visible(vis)
+
+    # Ticks 
+    ax.set_xticks(30 * np.arange(7))
+    ax2.set_xticks(30 * np.arange(7))
+    ax2.minorticks_on()
+    ax2.tick_params(which='minor', direction='in', left=False)
+    ax2.legend()
+
+    ax.set_xlabel(r'$\theta\,/\,{\rm deg}$')
+    ax.set_ylabel(r'$C_{{\rm GW}\times{\rm GRB}}(\theta)$')
+    ax.set_title('Angular cross-corelation between GW and GRB events')
+    
+    # Shift the sci. notation text to the left
+    exp = ax.yaxis.get_offset_text()
+    exp.set_x(-0.09)
 
     fig.savefig(FIG_DIR / 'Fig7_cross_correlation.pdf', dpi=DPI)
     return fig
