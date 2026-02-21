f5b770b822f3e0a588b05a88422d63f615df4dd8
Otto Akseli Hannuksela <otto.akseli.hannuksela@gmail.com>
2026-02-21 13:15:55 +0800

    Updated plot samples

--- production/2mass/2mass/plot_samples.py
+++ production/2mass/2mass/plot_samples.py
@@ -13,7 +13,11 @@ ra = np.deg2rad(data["RAJ2000"])
 dec = np.deg2rad(data["DEJ2000"])
 
 helperfunctions.plot_ra_dec(ra, dec)
-plt.show()
+plt.title("2MASS Galaxy Catalog", fontsize=14)
+plt.savefig("2mass_galaxy_catalog_spec_ra_dec.pdf", bbox_inches="tight")
+plt.close()
 
 helperfunctions.plot_l_b(ra, dec)
-plt.show()
+plt.title("2MASS Galaxy Catalog", fontsize=14)
+plt.savefig("2mass_galaxy_catalog_spec_l_b.pdf", bbox_inches="tight")
+plt.close()
